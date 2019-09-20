a65a1255599f23027a6b41e7c248bb19db76c234
Sarath Sasidharan <sarathsasidharan@Saraths-MacBook-Pro.local>
2019-09-20 12:09:12 +0200

    added setup file

---
+++ config/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+virtualenv env
+source venv/bin/activate
+pip install virtualenv
+sudo pip install virtualenv
+sudo pip install virtualenv
+virtualenv venv
+source venv/bin/activate
+pip install pyodbc
